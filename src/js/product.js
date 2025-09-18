847ea903dca1dfe4a4ca574d2554733a7622be7c
faharetana-rkt <rakotomaharitra@gmail.com>
2025-09-18 17:27:07 +0300

    added superscript backpack to product.js

--- src/js/product.js
+++ src/js/product.js
@@ -1,4 +1,4 @@
-import { getParam } from "./utils.mjs";
+import { getParam, renderNumberOfItemsBackpack } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
@@ -17,3 +17,6 @@ product.init();
 // document
 //   .getElementById("addToCart")
 //   .addEventListener("click", addToCartHandler);
+
+// function to render the superscript number of items in backpack
+renderNumberOfItemsBackpack(document.querySelector("#cart-numbers"), "so-cart");
