89383fc1afda504aba86f977b0ada13cfeab2ed2
faharetana-rkt <rakotomaharitra@gmail.com>
2025-09-21 08:17:33 +0300

    couldn't finish the individual

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,9 @@
-import ProductData from "./ProductData.mjs";
-import ProductList from "./PoductList.mjs";
 import { renderScrollingMessage, loadHeaderFooter, renderNumberOfItemsBackpack } from "./utils.mjs";
 
 // Wait until DOM is loaded before running scripts
 document.addEventListener("DOMContentLoaded", async () => {
   // Load header and footer
   await loadHeaderFooter();
-
-  // Initialize product list
-  const listElement = document.querySelector(".product-list");
-  const dataSource = new ProductData("tents");
-  const productList = new ProductList("tents", dataSource, listElement);
-  productList.init();
 
   // Render the superscript number of items in backpack
   renderNumberOfItemsBackpack(document.querySelector("#cart-numbers"), "so-cart");
