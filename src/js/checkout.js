5890646cb75ba2fd6e49f823d12f85c23f5c44c0
Gabriela <gabfrielaugartemaco@gmail.com>
2025-09-27 16:34:38 +0200

    group assignment w4 finished

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,4 +1,5 @@
 import { loadHeaderFooter, renderNumberOfItemsBackpack } from "./utils.mjs";
+import CheckoutProcess from "./CheckoutProcess.mjs";
 
 document.addEventListener("DOMContentLoaded", async () => {
   // Load header and footer
@@ -9,4 +10,19 @@ document.addEventListener("DOMContentLoaded", async () => {
     document.querySelector("#cart-numbers"),
     "so-cart",
   );
+  // Initialize checkout process
+  const checkout = new CheckoutProcess("so-cart", ".checkout-summary");
+  checkout.init();
+
+  // Calculate order total when ZIP code field loses focus
+  document.querySelector("#zip").addEventListener("blur", () => {
+    checkout.calculateOrderTotal();
+  });
+
+ 
+  document.forms["checkout"].addEventListener("submit", (e) => {
+    e.preventDefault();
+    checkout.checkout(e.target);
+  });
+
 });
